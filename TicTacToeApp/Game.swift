f5635710a1f274643e51c4f2700a9c929378f164
masamarojevic <marojevicmasa@gmail.com>
2023-09-19 11:51:39 +0200

    Logic and UI commit
    I ve added 2 function to Game class. One is for switching the current player and the second one is checking where at which index do we set a tag. in GameViewController I have a func onPress that takes that function created in Game class connects it with a UI tag and sets a symbol for it.   Func setSymbol checks who is the currentplayer and based on that it sets a corresponded image.

--- TicTacToeApp/Game.swift
+++ TicTacToeApp/Game.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class Game {
+    
+    var playerOne = 1
+    
+    var playerTwo = 2
+    
+    var currentPLayer = 1
+    
+    //we need a boolean to know if the game has started
+    var isPlaying = false
+    
+    var board = [0,0,0,0,0,0,0,0,0]
+    
+    func switchPlayers (){
+        if currentPLayer == 1{
+            currentPLayer = 2
+        }else{
+            currentPLayer =  1
+        }
+    }
+    
+    func setTag(atIndex: Int) -> Bool{
+        let index = atIndex - 1
+        guard atIndex >= 0 && atIndex < board.count && board[atIndex] == 0 else {
+            print("Invalid move at index \(index)")
+            return false
+        }
+        board[atIndex] = currentPLayer
+        switchPlayers()
+        return true
+    }
+    
+    
+}
--- TicTacToeApp/GameViewController.swift
+++ TicTacToeApp/GameViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var imgsq1: UIImageView!
+    @IBOutlet weak var imgsq2: UIImageView!
+    @IBOutlet weak var imgsq3: UIImageView!
+    @IBOutlet weak var imgsq4: UIImageView!
+    @IBOutlet weak var imgsq5: UIImageView!
+    @IBOutlet weak var imgsq6: UIImageView!
+    @IBOutlet weak var imgsq7: UIImageView!
+    @IBOutlet weak var imgsq8: UIImageView!
+    @IBOutlet weak var imgsq9: UIImageView!
+    
+    var game = Game()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setSymbol(tagNr: Int){
+      
+        let playerOneOrTwo = game.currentPLayer == game.playerOne ? "xmark"
+        : "circle"
+        let symbol = view.viewWithTag(tagNr) as? UIImageView
+        symbol?.image = UIImage(systemName: playerOneOrTwo)
     }
-    */
+   
+   
+    @IBAction func onPress(_ sender: UITapGestureRecognizer) {
+    guard let tag = sender.view?.tag else {return}
+        
+        if game.setTag(atIndex: tag){
+            setSymbol(tagNr: tag)
+        }
+        
 
+      print("tag nr : \(tag)")
+        
+    }
+    
+   
+    
 }
